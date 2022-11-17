df39a6c6917b50c2888ca68b038ca6e49d0ea07a
oguzhanascier <oguzhanascier20@gmail.com>
2022-11-17 12:10:21 +0300

    async

--- src/components/pokeCard.vue
+++ src/components/pokeCard.vue
@@ -2,14 +2,11 @@
   <div>
     <div class="card">
       <div class="pokeImg">
-        <img
-          :src="pokeImg"
-          alt=""
-        />
+        <img :src="pokeImg" alt="" />
       </div>
-      <div class="pokeId">#{{pokeId}}</div>
-      <div class="pokeName">{{pokeName}}</div>
-      <div class="pokeType">{{pokeType}}</div>
+      <div class="pokeId">#{{ pokeId }}</div>
+      <div class="pokeName">{{ pokeName }}</div>
+      <div class="pokeType">{{ pokeType }}</div>
     </div>
     <button @click="getPoke">POKE</button>
   </div>
@@ -21,32 +18,27 @@ export default {
   data() {
     return {
       pokeImg: null,
-      pokeId:null,
+      pokeId: null,
       pokeName: "at",
       pokeType: null,
-      random:null
+      random: null,
     };
   },
   methods: {
-   async getPoke() {
+    async getPoke() {
       let randomId = Math.floor(Math.random() * 905 + 1);
       this.random = randomId;
       console.log(this.random);
 
-      axios
-        .get("" + this.random)
-        .then((res) => {
-
-            console.log(res)
-          let pokemon = res.data;
-            this.pokeImg=pokemon
-            this.pokeId=pokemon.id
-            this.pokeName=pokemon.name
-            this.pokeType=pokemon.types[0].type.name
-            this.pokeImg=`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${this.pokeId}.png`
-
-
-        });
+     await (axios.get("" + this.random).then((res) => {
+        console.log(res);
+        let pokemon = res.data;
+        this.pokeImg = pokemon;
+        this.pokeId = pokemon.id;
+        this.pokeName = pokemon.name;
+        this.pokeType = pokemon.types[0].type.name;
+        this.pokeImg = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${this.pokeId}.png`;
+      }));
     },
   },
 };
