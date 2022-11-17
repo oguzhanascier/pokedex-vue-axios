84edde06522273319a4b6a766cb8712dc6c13c5a
oguzhanascier <oguzhanascier20@gmail.com>
2022-11-17 12:18:56 +0300

    pokebutton component

---
+++ src/components/pokeButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button class="changePoke">Random Pokemon</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
